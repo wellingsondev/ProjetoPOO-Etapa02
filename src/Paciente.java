33adb993e89c3a40dc291d41b2526dcdd80cec28
Lucas Gabriel <lucasgabri1602@icloud.com>
2026-06-22 21:21:26 -0300

    Refatora Paciente para herdar de Pessoa e utiliza super nos construtores

--- src/Paciente.java
+++ src/Paciente.java
@@ -1,92 +1,64 @@
-public class Paciente {
-    private String nome;
-    private String cpf;
+public class Paciente extends Pessoa { 
     private int idade;
-    private String telefone;
     private String convenioNome;
     private boolean ativo;
 
     public Paciente(String nome, String cpf) {
-        this.nome = nome;
-        this.cpf = cpf;
+        super(nome, cpf);
+
         this.idade = 0;
-        this.telefone = "";
+        setTelefone("");
         this.convenioNome = "";
         this.ativo = true;
     }
 
     public Paciente(String nome, String cpf, int idade, String telefone) {
-        this.nome = nome;
-        this.cpf = cpf;
+        super(nome, cpf);
+
         this.idade = idade;
-        this.telefone = telefone;
+        setTelefone(telefone);
         this.convenioNome = "";
         this.ativo = true;
     }
-
     // construtor com todos os dados
     public Paciente(String nome, String cpf, int idade, String telefone, String convenioNome) {
-        this.nome = nome;
-        this.cpf = cpf;
+        super(nome, cpf);
+
         this.idade = idade;
-        this.telefone = telefone;
+        setTelefone(telefone);
         this.convenioNome = convenioNome;
         this.ativo = true;
     }
-
     // atualiza so idade e telefone
     public void complementar(int idade, String telefone) {
         this.idade = idade;
-        this.telefone = telefone;
+        setTelefone(telefone);
     }
 
     // atualiza tudo incluindo convenio
     public void complementar(int idade, String telefone, String convenioNome) {
         this.idade = idade;
-        this.telefone = telefone;
+        setTelefone(telefone);
         this.convenioNome = convenioNome;
     }
 
     public void desativar() {
         this.ativo = false;
     }
-
+    
+    @Override
     public String exibirResumo() {
         String status = "Sim";
         if (!ativo) {
             status = "Nao";
         }
-        return "Nome: " + nome + " | CPF: " + cpf + " | Idade: " + idade
-                + " | Tel: " + telefone + " | Convenio: " + convenioNome
+        return "Nome: " + getNome() + " | CPF: " + getCpf() + " | Idade: " + idade
+                + " | Tel: " + getTelefone() + " | Convenio: " + convenioNome
                 + " | Ativo: " + status;
-    }
-
-    public String getNome(){
-        return nome;
-    }
-
-    public void setNome(String nome) {
-    if (!nome.equals("")) {
-        this.nome = nome;
-    }
-    }
-
-    public String getCpf() {
-    return cpf;
-    }
-
-    public void setCpf(String cpf) {
-    if (!cpf.equals("")) {
-        this.cpf = cpf;
-    }
     }
 
     public int getIdade() {
         return idade;
-    }
-
-    public String getTelefone() {
-        return telefone;
     }
 
     public String getConvenioNome() {
@@ -102,11 +74,6 @@ public class Paciente {
             this.idade = idade;
         }
     }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
     public void setConvenioNome(String convenioNome) {
         this.convenioNome = convenioNome;
     }
---
+++ src/Pessoa.java
@@ -0,0 +1,40 @@
+import java.util.Date;
+
+public abstract class Pessoa {
+
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private Date nascimento;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public abstract String exibirResumo();
+}
