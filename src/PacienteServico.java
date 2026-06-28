93fabfe1a841a6224762dc9af54a3affb777ae9b
JohnathaOlivDev <johnatha.oliveira.dev@gmail.com>
2026-06-28 03:14:33 +0000

    feat(jornada28): controle de unicidade de cpf com hashset

--- src/PacienteServico.java
+++ src/PacienteServico.java
@@ -1,15 +1,25 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class PacienteServico {
 
     private Map<String, Paciente> pacientesPorCpf;
+    private Set<String> cpfsCadastrados;
 
     public PacienteServico() {
         this.pacientesPorCpf = new HashMap<>();
+        this.cpfsCadastrados = new HashSet<>();
     }
 
-    public void cadastrar(Paciente paciente) {
+    public void cadastrar(Paciente paciente) throws CpfDuplicadoException {
+        boolean novoCpf = cpfsCadastrados.add(paciente.getCpf());
+
+        if (!novoCpf) {
+            throw new CpfDuplicadoException("o cpf " + paciente.getCpf() + " ja esta cadastrado");
+        }
+
         pacientesPorCpf.put(paciente.getCpf(), paciente);
     }
 
